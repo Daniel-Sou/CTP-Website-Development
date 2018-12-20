9933bde30cb1184c91e1e1a78089affb1238d4e0
Daniel Sou <42826881+Daniel-Sou@users.noreply.github.com>
2018-12-20 17:35:09 +0800

    Upload the Bootstrap and jQuery File
    CSS File
    1. dataTables.bootstrap.min.css
    
    JS File
    1. dataTables.bootstrap4.js
    2. jquery.dataTables.js
    3. jquery-3.3.1.slim.js

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#example').DataTable();
+} );
+
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
