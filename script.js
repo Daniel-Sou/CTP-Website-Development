e8f979e1ee66d4de9cb8395b852ca447d22985ee
Daniel Sou <42826881+Daniel-Sou@users.noreply.github.com>
2018-12-21 14:31:38 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
-
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
